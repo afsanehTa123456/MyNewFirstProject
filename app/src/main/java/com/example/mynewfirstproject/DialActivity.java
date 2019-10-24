61a78b9b3b5d03b7a1c50a3eadbe5ab4308697e0
afsaneh <hamrah2010@gmail.com>
2019-10-24 10:13:15 +0330

    changes for 4 session

---
+++ app/src/main/java/com/example/mynewfirstproject/DialActivity.java
@@ -0,0 +1,13 @@
+package com.example.mynewfirstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dial);
+    }
+}
